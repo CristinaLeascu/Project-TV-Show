7f72f2935e3a9fbb25608c1a19bf105b10e418ec
MarynaRomashca <marynarmnv@gmail.com>
2025-03-28 18:57:26 +0000

    200-search

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 //You can edit ALL of the code here
-
+let allEpisodes
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes); //!!!! reuse 
+  const numberTotal = document.getElementById("total");
+  numberTotal.textContent = `${allEpisodes.length}`;
 }
 
+
 function makePageForEpisodes(allEpisodes) {
+  const numberElem = document.getElementById("current");
+  numberElem.textContent = ` ${allEpisodes.length}`;
   const rootDiv = document.getElementById("root");
+  rootDiv.innerHTML = `                   
+  <template id="episode-card-template">
+    <section> 
+      <h3>Film title</h3>
+      <img />
+      <p>Summary</p>
+    </section>
+  </template>
+  `;
+// HTML clean 
 
   for (let i = 0; i < allEpisodes.length; i++) {
     const episodeCard = document
@@ -38,9 +53,22 @@ function makePageForEpisodes(allEpisodes) {
     );
     console.log(episodeSummary);
     episodeCard.querySelector("p").textContent = `${episodeSummary}`;
-
     rootDiv.append(episodeCard);
   }
 }
+
+document.getElementById("myInput").addEventListener("input", searchEpisode);
+
+function searchEpisode(event) {
+  const searchText = event.target.value.trim(); 
+  const foundEpisode = allEpisodes.filter(ep => ep.name.includes(searchText) || ep.summary.includes(searchText)); 
+
+  if (foundEpisode) {
+      makePageForEpisodes (foundEpisode);
+  } else {
+  }
+}
+
+
 
 window.onload = setup;
